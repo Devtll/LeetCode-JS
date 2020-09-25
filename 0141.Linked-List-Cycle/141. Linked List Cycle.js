6c78b1cea978d82968c6529888d8c2d7d6073932
tuojl <tuojialin@diligrp.com>
2020-09-25 10:38:11 +0800

    resolved 141. Linked List Cycle

--- 0141.Linked-List-Cycle/141. Linked List Cycle.js
+++ 0141.Linked-List-Cycle/141. Linked List Cycle.js
@@ -14,10 +14,10 @@ var hasCycle = function (head) {
   if (head === null || head.next === null) return false
   let slow = head
   let fast = head.next
-  while (fast !== null) {
-    if (false === slow) return true
-    fast = fast.next
+  while (slow !== fast) {
+    if (fast === null || fast.next === null) return false
+    fast = fast.next.next
     slow = slow.next
   }
-  return false
+  return true
 }
